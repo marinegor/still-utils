aea442a2e2f1baad4afee829ceb686bbfca67513
Egor Marin <marin@phystech.edu>
2020-09-22 01:06:09 +0300

    Rename main folder and change binaries mask

--- setup.py
+++ setup.py
@@ -1,24 +1,17 @@
 from setuptools import setup
 import glob
 
-requirements = """\
-numpy==1.18.0
-matplotlib==3.1.2
-rmsd==1.3.2
-h5py==2.10.0
-scipy==1.4.1
-tqdm==4.19.5
-filelock==3.0.12
-PyYAML==5.3.1"""
-
-scripts = glob.glob("./crystfeler/sh/*er") + glob.glob("./crystfeler/py/*")
+scripts = glob.glob("./still-utils/bin/*er") + glob.glob("./still-utils/bin/*.py")
+with open("requirements.txt", "r") as fin:
+    requirements = fin.split("\n")
 
 setup(
-    name="crystfeler",
+    name="still-utils",
     version="0.1",
     description="Module full of CLI tools to support your SSX/SFX data processing",
     author="Egor Marin",
     author_email="marin@phystech.edu",
-    install_requires=requirements.split("\n"),  # external packages as dependencies
+    # external packages as dependencies
+    install_requires=requirements,
     scripts=scripts,
 )
