327329cf4198da7b29715fd5ac493b24ebef4bd4
Egor Marin <marin@phystech.edu>
2020-09-22 12:07:49 +0300

    Change package name to stilutils and update vesrions to >= instead of ==

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import glob
 
-scripts = glob.glob("./still-utils/bin/*er") + glob.glob("./still-utils/bin/*.py")
+scripts = glob.glob("./stilutils/bin/*")
 with open("requirements.txt", "r") as fin:
-    requirements = fin.split("\n")
+    fin = fin.read()
+    requirements = fin.replace("==", ">=").split("\n")
 
 setup(
-    name="still-utils",
-    version="0.1",
+    name="stilutils",
     description="Module full of CLI tools to support your SSX/SFX data processing",
     author="Egor Marin",
     author_email="marin@phystech.edu",
     # external packages as dependencies
     install_requires=requirements,
     scripts=scripts,
+    packages=find_packages(),
 )
